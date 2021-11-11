e9678cdd73a565698ce9400a792d42de3878d63e
Christian <c.herrera14@ufromail.cl>
2021-11-11 13:54:31 -0300

    4to

---
+++ services/nodemailer.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer')
+// initialize nodemailer
+var transporter = nodemailer.createTransport(
+    {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'brmb-ufro@ufrontera.cl',
+            pass: 'BRmB2021'
+        }
+    }
+);
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
+
+module.exports = transporter
